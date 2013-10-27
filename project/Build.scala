02f7948fae93ef483b61ca3612cbf2f1ff5b8da4
Malik Baktiyarov <malik@baktiyarov.com>
2013-10-27 22:19:01 +0400

    protostuff

--- project/Build.scala
+++ project/Build.scala
@@ -12,25 +12,33 @@ object PokernoBuild extends Build {
     scalaVersion := "2.10.3"
   )
 
-  lazy val dependencies = Seq(
-    //  "org.scalaz" %% "scalaz-core" % "7.0.3",
+  lazy val deps = Seq(
+    //"org.scalaz" %% "scalaz-core" % "7.0.3",
     "com.twitter" %% "finagle-core" % "6.6.2",          
     "com.twitter" %% "finagle-http" % "6.6.2",
+    
     "com.typesafe.akka" %% "akka-actor" % "2.2.1",
     "com.typesafe.akka" %% "akka-zeromq" % "2.2.1",
     "com.typesafe.akka" %% "akka-testkit" % "2.2.1",
+    
     "org.webbitserver" % "webbit" % "0.4.15",
+    
     "asia.stampy" % "stampy-core" % "1.0-RELEASE",
+    
     "com.github.scopt" %% "scopt" % "3.1.0",
-    //  "org.msgpack" %% "msgpack-scala" % "0.6.8",
-    "org.msgpack" % "msgpack" % "0.6.8",
-    //  "com.twitter" % "ostrich" % "2.3.0"
+    //"com.twitter" % "ostrich" % "2.3.0"
     "com.codahale.metrics" % "metrics-core" % "3.0.1",
-    "org.slf4j" % "slf4j-api" % "1.6.4" % "provided",
-    "org.scala-lang" % "scalap" % "2.10.3"
+    
+    "org.msgpack" %% "msgpack-scala" % "0.6.8",
+    //"org.msgpack" % "msgpack" % "0.6.8",
+    "com.dyuproject.protostuff" % "protostuff-core" % "1.0.7",
+    "com.dyuproject.protostuff" % "protostuff-json" % "1.0.7",
+    "com.dyuproject.protostuff" % "protostuff-api" % "1.0.7",
+    "com.dyuproject.protostuff" % "protostuff-runtime" % "1.0.7",
+    "com.dyuproject.protostuff" % "protostuff-compiler" % "1.0.7"
   )
   
-  lazy val dependenciesTest = Seq(
+  lazy val testDeps = Seq(
     "org.scalatest" %% "scalatest" % "1.9.2" % "test"
   )
   
@@ -38,8 +46,8 @@ object PokernoBuild extends Build {
     id = "pokerno",
     base = file("."),
     settings = Project.defaultSettings ++ Seq(
-      libraryDependencies ++= dependencies,
-      libraryDependencies ++= dependenciesTest
+      libraryDependencies ++= deps,
+      libraryDependencies ++= testDeps
     )
   )
 }

e6b27aa4f58b67edf55f4ca32f8e895b52ff27fa
Malik Baktiyarov <malik@baktiyarov.com>
2013-12-26 02:55:45 +0400

    ...

--- pokerno-server/src/main/scala/de/pokerno/backend/server/Poker.scala
+++ pokerno-server/src/main/scala/de/pokerno/backend/server/Poker.scala
@@ -7,7 +7,7 @@ import com.twitter.finagle.http.Http
 import org.jboss.netty.handler.codec.http._
 import java.net.InetSocketAddress
 
-import de.pokerno.poker._
+import de.pokerno.poker
 import de.pokerno.poker.{ Math ⇒ PokerMath }
 
 object Poker {
@@ -20,31 +20,31 @@ object Poker {
     case class generateDeckRq()
     case class generateDeckResp(cards: String)
     def generateDeck(r: generateDeckRq) = {
-      Future.value(generateDeckResp(cards = Cards(Deck())))
+      Future.value(generateDeckResp(cards = poker.Cards(poker.Deck())))
     }
 
     // evaluate hand
     case class evaluateRq(cards: String)
-    case class evaluateResp(hand: Hand)
+    case class evaluateResp(hand: poker.Hand)
 
     def evaluateHand(r: evaluateRq) = {
-      val cards = Cards(r.cards)
-      val hand = Hand.High(cards)
+      val cards = poker.Cards(r.cards)
+      val hand = poker.Hand.High(cards)
 
       Future value (evaluateResp(hand = hand.get))
     }
 
     // compare two hands
     case class compareHandsRq(a: String, b: String, board: String)
-    case class compareResp(h1: Hand, h2: Hand, result: Int)
+    case class compareResp(h1: poker.Hand, h2: poker.Hand, result: Int)
 
     def compareHands(r: compareHandsRq) = {
-      val a = Cards(r.a)
-      val b = Cards(r.b)
-      val board = Cards(r.board)
+      val a = poker.Cards(r.a)
+      val b = poker.Cards(r.b)
+      val board = poker.Cards(r.board)
 
-      val h1 = Hand.High(a ++ board)
-      val h2 = Hand.High(b ++ board)
+      val h1 = poker.Hand.High(a ++ board)
+      val h2 = poker.Hand.High(b ++ board)
 
       Future value (compareResp(h1 = h1.get, h2 = h2.get, result = h1.get compare (h2 get)))
     }
@@ -54,27 +54,17 @@ object Poker {
     case class simulateResp(sample: PokerMath.Sample)
 
     def simulateHands(r: simulateHandsRq) = {
-      val a = Cards(r.a)
-      val b = Cards(r.b)
-      val board = Cards(r.board)
+      val a = poker.Cards(r.a)
+      val b = poker.Cards(r.b)
+      val board = poker.Cards(r.board)
 
       val hu = PokerMath.Headsup(a, b, r.samples)
       Future value (simulateResp(sample = hu withBoard (board)))
     }
   }
-
-  val service: finagle.Service[HttpRequest, HttpResponse] = new Service
-
-  object Config {
-    var name = "poker_service"
-    var host: String = "localhost"
-    var port: Int = 8081
-  }
 }
 
 class Poker extends Runnable {
   def run {
-    val address = new InetSocketAddress(Node.Config.host, Node.Config.port)
-    val server: Server = ServerBuilder().codec(Http()).bindTo(address).name(Node.Config.name) build (Node.service)
   }
 }

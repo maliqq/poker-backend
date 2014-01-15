951a98281cabed2e7def5fcb87c8724c6a74642e
Malik Baktiyarov <malik@baktiyarov.com>
2014-01-15 19:09:31 +0400

    parser

--- src/main/scala/de/pokerno/format/text/Lexer.scala
+++ src/main/scala/de/pokerno/format/text/Lexer.scala
@@ -1,33 +1,170 @@
 package de.pokerno.format.text
 
+import de.pokerno.model
+import de.pokerno.poker
+import util.matching.Regex
+
 object Lexer {
-  class Variation
-  class Limit
-  class BetType
+  case class Tag(name: String) extends annotation.StaticAnnotation
   
-  object Tag {
-    class Table(uuid: String, max: Int)
-    class Seat(playerUuid: String, stack: Int)
-    class Stake(sb: Int, bb: Int, ante: Option[Int] = None)
-    class Game(variation: Variation, limit: Limit)
-    class Speed(interval: Int)
-    class Button(pos: Int)
-    class Street(name: String)
-    class Bet(`type`: BetType, player: String)
-    class Sb(player: String)
-    class Bb(player: String)
-    class Ante(player: String)
-    class Raise(player: String, amount: Int)
-    class AllIn(player: String)
-    class Call(player: String, amount: Int)
-    class Fold(player: String)
-    class Deal(player: String, cardsNum: Int, cards: String)
-    class Json(data: String)
-    class Chat(player: String, message: String)
-    class Discard(player: String, cards: String)
-    class Show(player: String, cards: String)
-    class Muck(player: String, cards: String)
-    class Sleep(time: Int)
-    class Timer(player: String, left: Int)
+  object Conversions {
+    implicit def string2int(s: String) = Integer.parseInt(s)
+    implicit def string2cards(s: String) = poker.Cards(s)
+    implicit def string2quotedString(s: String) = new QuotedString(s)
   }
+  
+  class QuotedString(v: String) {
+    override def toString = v
+  }
+  
+  trait Token
+  
+  object Tags {
+    import Conversions._
+    
+    @Tag(name = "TABLE")
+    case class Table(uuid: QuotedString, max: Int) extends Token {
+      def this(params: Array[String]) = this(params(0), params(1).replace("-max", ""))
+    }
+    
+    @Tag(name = "SEAT")
+    case class Seat(playerUuid: QuotedString, stack: Int) extends Token {
+      def this(params: Array[String]) = this(params(0), params(1))
+    }
+    
+    object StakeUtil {
+      def fromParams(params: Array[String]): Tuple3[Int, Int, Option[Int]] = {
+        val d = params(0).split("/")
+        println("HERE")
+        d.length match {
+          case 2 =>
+            (d(0), d(1), None)
+          case 3 =>
+            (d(0), d(1), Some(d(2)))
+          case _ =>
+            throw new IllegalArgumentException("STAKE requires 2 or 3 arguments")
+        }
+      }
+    }
+    
+    @Tag(name = "STAKE")
+    case class Stake(sb: Int, bb: Int, ante: Option[Int]) extends Token {
+      def this(args: Tuple3[Int, Int, Option[Int]]) = this(args._1, args._2, args._3)
+      def this(params: Array[String]) = this(StakeUtil.fromParams(params))
+    }
+    
+    @Tag(name = "GAME")
+    case class Game(variation: model.Variation, limit: model.Game.Limit) extends Token {
+      def this(params: Array[String]) = this(null, null)
+    }
+    
+    @Tag(name = "SPEED")
+    case class Speed(interval: Int) extends Token {
+      def this(params: Array[String]) = this(params(0))
+    }
+    
+    @Tag(name = "BUTTON")
+    case class Button(pos: Int) extends Token {
+      def this(params: Array[String]) = this(params(0))
+    }
+    
+    @Tag(name = "STREET")
+    case class Street(name: String) extends Token {
+      def this(params: Array[String]) = this(params(0))
+    }
+    
+    @Tag(name = "SB")
+    case class Sb(player: QuotedString) extends Token {
+      def this(params: Array[String]) = this(params(0))
+    }
+    
+    @Tag(name = "BB")
+    case class Bb(player: QuotedString) extends Token {
+      def this(params: Array[String]) = this(params(0))
+    }
+    
+    @Tag(name = "ANTE")
+    case class Ante(player: QuotedString) extends Token {
+      def this(params: Array[String]) = this(params(0))
+    }
+    
+    @Tag(name = "RAISE")
+    case class Raise(player: QuotedString, amount: Int) extends Token {
+      def this(params: Array[String]) = this(params(0), params(1))
+    }
+    
+    @Tag(name = "ALLIN")
+    case class AllIn(player: QuotedString) extends Token {
+      def this(params: Array[String]) = this(params(0))
+    }
+    
+    @Tag(name = "CALL")
+    case class Call(player: QuotedString, amount: Int) extends Token {
+      def this(params: Array[String]) = this(params(0), params(1))
+    }
+    
+    @Tag(name = "FOLD")
+    case class Fold(player: QuotedString) extends Token {
+      def this(params: Array[String]) = this(params(0))
+    }
+    
+    @Tag(name = "DEAL")
+    case class Deal(player: QuotedString, cards: List[poker.Card], cardsNum: Integer) extends Token {
+      def this(params: Array[String]) = this(params(0), params(1), null)
+    }
+    
+    @Tag(name = "JSON")
+    case class Json(data: QuotedString) extends Token {
+      def this(params: Array[String]) = this(params(0))
+    }
+    
+    @Tag(name = "CHAT")
+    case class Chat(player: QuotedString, message: String) extends Token {
+      def this(params: Array[String]) = this(params(0), params(1))
+    }
+    
+    @Tag(name = "DISCARD")
+    case class Discard(player: QuotedString, cards: List[poker.Card]) extends Token {
+      def this(params: Array[String]) = this(params(0), params(1))
+    }
+    
+    @Tag(name = "SHOW")
+    case class Show(player: String, cards: List[poker.Card]) extends Token {
+      def this(params: Array[String]) = this(params(0), params(1))
+    }
+    
+    @Tag(name = "MUCK")
+    case class Muck(player: QuotedString, cards: List[poker.Card]) extends Token {
+      def this(params: Array[String]) = this(params(0), params(1))
+    }
+    
+    @Tag(name = "SLEEP")
+    case class Sleep(time: Int) extends Token {
+      def this(params: Array[String]) = this(params(0))
+    }
+    
+    @Tag(name = "TIMER")
+    case class Timer(player: QuotedString, left: Int) extends Token {
+      def this(params: Array[String]) = this(params(0), params(1))
+    }
+  }
+  
+  
+  import reflect.runtime.{ universe => ru }
+  import reflect.runtime.{ currentMirror => cm }
+  
+  val tagsReflection = cm.reflect(Tags)
+  val tags = tagsReflection.symbol.typeSignature.declarations.filter(_.isClass)
+  val tagClasses = tags.map { decl =>
+    val name = decl.annotations.head.scalaArgs(0).asInstanceOf[ru.Literal]
+    name match {
+      case ru.Literal(ru.Constant(s: String)) =>
+        s -> decl
+      case _ =>
+        name.toString -> decl
+    }
+  }.toMap
+  
+  Console printf("tagClasses=%s\n", tagClasses)
+
 }
--- src/main/scala/de/pokerno/format/text/Parser.scala
+++ src/main/scala/de/pokerno/format/text/Parser.scala
@@ -1,5 +1,51 @@
 package de.pokerno.format.text
 
-object Parser {
+import util.matching.Regex
 
+object Parser {
+  
+  def parseTagAndParams(line: String): Tuple2[String, Array[String]] = {
+    val p = """^([A-Z]+)(?:\s+(.+))?$""".r
+    line.stripLineEnd match {
+      case p(t: String, s: String) =>
+        (t, """\s""".r.split(s))
+      case s: String =>
+        (s, null)
+      case _ =>
+        (null, null)
+    }
+  }
+  
+  import reflect.runtime.{ currentMirror => cm }
+  import reflect.runtime.{ universe => ru }
+  
+  def parseLine(line: String): Option[Lexer.Token] = {
+    if (line.startsWith("#") || line.matches("^\\s*$")) return None
+    val parts = line.split("#", 2)
+    val (tag, params) = parseTagAndParams(parts(0))
+    if (tag == null) return None
+    
+    val decl = Lexer.tagClasses.get(tag)
+    if (decl.isDefined) {
+      try {
+        val m = decl.get.typeSignature.declaration(ru.nme.CONSTRUCTOR).asTerm.alternatives.last
+        val result = cm.reflectClass(decl.get.asClass).reflectConstructor(m.asMethod)(params)
+        return Some(result.asInstanceOf[Lexer.Token])
+      } catch {
+      case e: Throwable =>
+        e.printStackTrace()
+        Console printf("can't parse: %s with type: %s\n", line, decl.get)
+        return None
+      }
+    } else None
+  }
+  
+  def parse(lines: List[String]): List[Lexer.Token] = {
+    lines.map(parseLine(_)).filter(_.isDefined).map(_.get)
+  }
+  
+  def parse(src: scala.io.Source): List[Lexer.Token] = {
+    parse(src.getLines.toList)
+  }
+  
 }

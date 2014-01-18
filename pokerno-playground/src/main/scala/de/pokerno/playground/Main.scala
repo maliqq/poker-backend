4112fd4fb4893b0aff88c28b8d6bfb5120e16621
Malik Baktiyarov <malik@baktiyarov.com>
2014-01-18 22:58:23 +0400

    ...

--- pokerno-playground/src/main/scala/de/pokerno/playground/Main.scala
+++ pokerno-playground/src/main/scala/de/pokerno/playground/Main.scala
@@ -2,15 +2,34 @@ package de.pokerno.playground
 
 import de.pokerno.backend.gateway.{Http, http}
 import de.pokerno.format.text
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.pattern.ask
+import akka.util.Timeout
+import concurrent.Await
+import concurrent.duration._
 
 object Main {
   val system = ActorSystem("poker-playground")
   
+  implicit val timeout = Timeout(5 seconds)
+  
+  class Test extends Actor {
+    def receive = {
+      case m: Any =>
+        Console printf("got: %s\n\n", m)
+    }
+  }
+  
   def main(args: Array[String]) = {
-    val htmlEventSource = system.actorOf(Props(classOf[Http.Gateway],
-        http.Config(port = 8080, eventSource = Right(true))))
+    //val htmlEventSource = system.actorOf(Props(classOf[Http.Gateway],
+    //    http.Config(port = 8080, eventSource = Right(true))))
     
+    val a = system.actorOf(Props(classOf[Test]))
+    val b = system.actorOf(Props(classOf[Replayer], a))
     
+    text.Parser.parse(scala.io.Source.fromFile("deal.txt")).foreach { tag =>
+      Console printf("%s%s%s\n", Console.GREEN, tag, Console.RESET)
+      b ! tag
+    }
   }
 }
--- pokerno-playground/src/main/scala/de/pokerno/playground/Replayer.scala
+++ pokerno-playground/src/main/scala/de/pokerno/playground/Replayer.scala
@@ -1,74 +1,108 @@
 package de.pokerno.playground
 
 import de.pokerno.format.text.Lexer.{Tags => tags}
-import de.pokerno.model
+import de.pokerno.model.{Player, Table, Stake, Variation, Game, Bet}
 import de.pokerno.protocol._
-import akka.actor.{Actor, ActorRef}
+import wire.Conversions._
+import akka.actor.{Actor, ActorLogging, ActorRef}
 
-class Replayer(node: ActorRef) extends Actor {
+class Replayer(node: ActorRef) extends Actor with ActorLogging {
   import context._
-
-  var table: Option[model.Table] = None
-  var stake: Option[model.Stake] = None
+  
+  var table: Option[Table] = None
+  var stake: Option[Stake] = None
+  var variation: Option[Variation] = None
   var id: Option[String] = None
   var speed: Option[Int] = None
-  var variation: Option[model.Variation] = None
-  var limit: Option[model.Game.Limit] = None
-
+  
   override def preStart {
-
   }
   
   override def receive = main
 
   def main: Receive = {
-    case tags.Table(_id, size) =>
-      table = Some(new model.Table(size))
+    case x: Any =>
+      Console printf("x=%s\n", x)
+      x match {
+      case tags.Table(_id, size) =>
+      table = Some(new Table(size))
       id = Some(_id.unquote)
+      println("tableBlock")
       become(tableBlock)
       
     case tags.Speed(duration) =>
       speed = Some(duration)
       
     case tags.Street(name) =>
+      println("streetBlock")
       become(streetBlock)
       
     case _ =>
 
+  }}
+  
+  def bet(player: String, bet: Bet) {
+    Console printf("BET: %s", bet)
+    table.map { t =>
+      t.seats.where(_.player.get.toString == player).map { case (seat, pos) =>
+        node ! rpc.AddBet(seat.player.get, bet)
+      }
+    }
   }
 
-  def streetBlock: Receive = {
+  def streetBlock: Receive = {case x: Any =>
+    Console printf("x=%s\n", x)
+    x match {
     case tags.Sb(player) =>
+      bet(player.unquote, Bet.sb(stake.get.smallBlind))
+      
     case tags.Bb(player) =>
+      bet(player.unquote, Bet.bb(stake.get.smallBlind))
+      
     case tags.Ante(player) =>
+      bet(player.unquote, Bet.ante(stake.get.smallBlind))
+      
     case tags.Raise(player, amount) =>
+      bet(player.unquote, Bet.raise(amount))
+      
     case tags.AllIn(player) =>
+      bet(player.unquote, Bet.allin)
+      
     case tags.Call(player, amount) =>
+      bet(player.unquote, Bet.call(amount))
+      
     case tags.Fold(player) =>
+      bet(player.unquote, Bet.fold)
+      
     case tags.Deal(player, cards, cardsNum) =>
       
     case x: Any =>
-      become(main)
+      println("main from streetBlock")
       self ! x
-  }
+      become(main)
+  }}
 
-  import wire.Conversions._
-  
-  def tableBlock: Receive = {
+  def tableBlock: Receive = {case x:Any =>
+    Console printf("x=%s\n", x)
+    x match {
     case tags.Seat(uuid, stack) =>
-      table.map(_.button.move)
+      table.map { t =>
+        val player = Player(uuid.unquote)
+        t.addPlayer(player, t.button, Some(stack))
+        //node ! rpc.JoinPlayer(player, t.button, Some(stack))
+        t.button.move
+      }
       
     case tags.Stake(sb, bb, ante) =>
-      stake = Some(model.Stake(sb,
+      stake = Some(Stake(sb,
           Some(bb),
           Ante = ante match {
             case Some(n) => Left(n)
             case None => Right(false)
           }))
           
-    case tags.Game(_variation, _limit) =>
-      variation = Some(_variation)
-      limit = Some(_limit)
+    case tags.Game(game, limit) =>
+      variation = Some(Game(game, limit))
       
     case tags.Button(pos) =>
       table.map(_.button.current = pos)
@@ -78,9 +112,10 @@ class Replayer(node: ActorRef) extends Actor {
           table = wire.Table(table.size),
           variation = variation.get,
           stake = stake.get)
-      become(main)
+      println("main from tableBlock")
       self ! x
-  }
+      become(main)
+  }}
 
   override def postStop {
 

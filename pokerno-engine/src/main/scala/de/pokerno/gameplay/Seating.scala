d7164b2249d168a3a3b85990bf451dac0322c5a8
Malik Baktiyarov <malik@baktiyarov.com>
2014-02-28 01:42:04 +0400

    check inactive users before deal

--- pokerno-engine/src/main/scala/de/pokerno/gameplay/Seating.scala
+++ pokerno-engine/src/main/scala/de/pokerno/gameplay/Seating.scala
@@ -1,18 +1,22 @@
 package de.pokerno.gameplay
 
 import de.pokerno.model.{ Player, Seat }
+import concurrent.duration._
 
 private[gameplay] trait Seating {
   ctx: ContextLike ⇒
 
   def prepareSeats(ctx: StageContext) {
 
-    table.seatsAsList.foreach { seat ⇒
+    table.seatsAsList.zipWithIndex.foreach { case (seat, pos) ⇒
       if (seat.canPlayNextDeal) seat.play()
       if (seat.isAllIn) seat.idle()
+      if (seat.lastSeenOnlineBefore(System.currentTimeMillis() - 10.minutes.toMillis)) {
+        table.clearSeat(pos)
+      }
       seat.clearPut()
     }
-
+    
   }
 
 }

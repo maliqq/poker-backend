78c15e819ee3edfd8838cbd6d4ae1d6bd5f89dd8
Malik Baktiyarov <malik@baktiyarov.com>
2014-02-28 15:01:40 +0400

    betting timer

---
+++ pokerno-engine/src/main/scala/de/pokerno/gameplay/BettingTimer.scala
@@ -0,0 +1,27 @@
+package de.pokerno.gameplay
+
+import akka.actor.{Actor, Cancellable}
+import concurrent.duration._
+
+trait BettingTimer {
+  
+  a: Actor =>
+    
+  import context._
+
+  var maxTicks: Long = 30 
+  var timer: Cancellable = null
+  var ticks: Long = 0
+  
+  def startTimer() {
+    timer = system.scheduler.schedule(1 seconds, 1 seconds) {
+      ticks += 1
+      if (ticks >= maxTicks) {
+        timer.cancel()
+        ticks = 0
+        self ! Betting.Timeout
+      }
+    }
+  }
+  
+}

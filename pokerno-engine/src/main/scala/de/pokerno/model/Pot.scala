0b616805b1c80311d24f330b310e3a316a15179c
Malik Baktiyarov <malik@baktiyarov.com>
2014-06-26 01:59:37 +0400

    Pot, SidePot

--- pokerno-engine/src/main/scala/de/pokerno/model/Pot.scala
+++ pokerno-engine/src/main/scala/de/pokerno/model/Pot.scala
@@ -1,98 +1,7 @@
 package de.pokerno.model
 
-import java.util.Locale
 import util.control.Breaks._
 import com.fasterxml.jackson.annotation.{ JsonValue, JsonIgnore, JsonProperty, JsonPropertyOrder }
-
-class SidePot(val capFrom: Decimal = 0, val cap: Option[Decimal] = None) {
-  // members
-  var members = collection.mutable.Map[Player, Decimal]()
-  
-  def isMember(member: Player)  = members.contains(member)
-  @JsonValue def total: Decimal = members.values.sum
-  def isActive: Boolean         = members.size > 0 && total > .0
-
-  def add(member: Player, left: Decimal): Decimal = add(member, left, left)
-
-  def add(member: Player, _amount: Decimal, left: Decimal): Decimal = {
-    if (left == 0) return 0
-
-    val value: Decimal = members.getOrElse(member, 0)
-    val amount = value + _amount
-    val newValue = value + left
-
-    if (!cap.isDefined) {
-      members(member) = newValue
-      return 0
-    }
-
-    val capAmount = cap.get
-    if (amount >= capFrom && capAmount <= amount) { // (capAmount > value) {
-      members(member) = capAmount
-      return newValue - capAmount
-    }
-
-    left
-  }
-  
-  def split(member: Player, left: Decimal): Tuple2[SidePot, SidePot] = split(member, left, left)
-
-  def split(member: Player, _amount: Decimal, left: Decimal, cap: Option[Decimal] = None): Tuple2[SidePot, SidePot] = {
-    val value: Decimal = members getOrElse (member, 0)
-    val bet = value + left
-    val amount = value + _amount
-
-    members(member) = bet
-
-    val _new = new SidePot(amount, cap map { capAmount ⇒
-      capAmount - bet
-    } orElse (Some(amount)))
-
-    _new.members = members
-      .filter { case (key, _value) ⇒ _value > bet && key != member }
-      .map { case (key, _value) ⇒ (key, _value - bet) }
-
-    val _old = new SidePot(capFrom, Some(bet))
-
-    _old.members = members map {
-      case (key, _value) ⇒
-        (key, List(_value, bet).min)
-    }
-
-    (_new, _old)
-  }
-  
-  def uncalled(): Option[Tuple2[Player, Decimal]] = {
-    if (members.size == 1) {
-      return members.headOption
-    }
-    if (members.size > 1) {
-      val sorted = members.toSeq.sortBy(_._2).reverse
-      val max = sorted.head // max bet
-      val (called, under) = sorted.span(_._2 == max._2) // how much called max bet
-      if (called.size == 1)
-        return Some(max._1, max._2 - under.head._2)
-    }
-    None
-  }
-
-  override def toString = {
-    val s = new StringBuilder
-
-    s.append(
-      members.map {
-        case (member, amount) ⇒
-          "* %s:\t%.2f" formatLocal (Locale.US, member, amount)
-      }.mkString("\n")
-    )
-    s.append(" = %.2f" formatLocal (Locale.US, total))
-    //if (cap.isDefined)
-    s.append(" (cap: %.2f+%.2f)" formatLocal (Locale.US, capFrom, cap.getOrElse(.0)))
-
-    s.append("\n")
-    s.toString()
-  }
-}
 
 @JsonPropertyOrder(Array("total", "side"))
 class Pot {
@@ -148,15 +57,9 @@ class Pot {
     //Console printf("main=%s\nside=%s", main, side)
   }
   
-  def complete() = {
-    val uncalled = main.uncalled()
-    // adjust with uncalled bet
-    uncalled.map { case (member, amount) =>
-      main.members(member) -= amount
-    }
+  def complete() {
     if (main.isActive) _side ++= List(main)
     main = new SidePot
-    uncalled
   }
 
 //  def complete() {

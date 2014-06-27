4e1e1615c29dfe4c54f4dec76629453fd2e2afbe
Malik Baktiyarov <malik@baktiyarov.com>
2014-06-27 15:04:00 +0400

    ...

--- pokerno-data/src/main/scala/de/pokerno/data/pokerdb/PokerDB.scala
+++ pokerno-data/src/main/scala/de/pokerno/data/pokerdb/PokerDB.scala
@@ -193,10 +193,13 @@ object PokerDB extends Schema {
   sealed case class TournamentBuyIn(
       @Column(name = "currency_id", optionType=classOf[Long]) var currencyId: Option[Long], 
       var price: Double,
-      var fee: Double
+      var fee: Double,
+      @Column("starting_stack") var startingStack: Int,
+      @Column("addonStack") var addonStack: Int,
+      @Column(name = "bounty", optionType = classOf[Double]) var bounty: Option[Double]
     ) {
     var id: Long = 0
-    def this() = this(None, 0, 0) 
+    def this() = this(None, 0, 0, 0, 0, None)
   }
   
   val nodes     = table[Node]("nodes")

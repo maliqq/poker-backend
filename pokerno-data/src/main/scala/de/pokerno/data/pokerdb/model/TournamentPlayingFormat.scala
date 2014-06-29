8833b20a2185d74a93d4e8302af7221ee36d1f7b
Malik Baktiyarov <malik@baktiyarov.com>
2014-06-29 21:35:48 +0400

    typo fix

--- pokerno-data/src/main/scala/de/pokerno/data/pokerdb/model/TournamentPlayingFormat.scala
+++ pokerno-data/src/main/scala/de/pokerno/data/pokerdb/model/TournamentPlayingFormat.scala
@@ -8,8 +8,8 @@ sealed case class TournamentPlayingFormat(
   @Column("freezout") isFreeout: Boolean = false,
   @Column("knockout") isKnockout: Boolean = false,
   @Column("shootout") isShootout: Boolean = false,
-  @Column("rebuys") hasRebuys: Boolean = false.
-  @Column("addons") hasAddons: Boolean = false
+  @Column("rebuys") hasRebuys: Boolean = false,
+  @Column("addons") hasAddons: Boolean = false,
   @Column(name = "rebuys_limit", optionType = classOf[Int]) maxRebuys: Option[Int] = None
   ) {
   var id: Long = 0

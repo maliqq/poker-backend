dc4b94d7843634b77265db81e9913c2b6913f89f
Malik Baktiyarov <malik@baktiyarov.com>
2014-07-06 04:54:43 +0400

    ...

--- pokerno-engine/src/main/scala/de/pokerno/gameplay/tournament/Bubble.scala
+++ pokerno-engine/src/main/scala/de/pokerno/gameplay/tournament/Bubble.scala
@@ -4,3 +4,11 @@ object Bubble {
   case object Pause
   case object Resume
 }
+
+trait Bubble {
+  
+  var bubbleEnded: Option[java.time.Instant]
+  def endBubble() = bubbleEnded = Some(java.time.Instant.now())
+  def passedBubble() = bubbleEnded.isDefined
+  
+}

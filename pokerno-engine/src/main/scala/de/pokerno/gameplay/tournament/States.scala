2f474b62680730660c2daecbd6a896fa6760a073
Malik Baktiyarov <malik@baktiyarov.com>
2014-07-06 05:01:37 +0400

    states

---
+++ pokerno-engine/src/main/scala/de/pokerno/gameplay/tournament/States.scala
@@ -0,0 +1,18 @@
+package de.pokerno.gameplay.tournament
+
+object States {
+  trait Value
+
+  // waiting players
+  case object Waiting extends Value
+  
+  // seating
+  case object Seating extends Value
+  
+  // play is active
+  case object Running extends Value
+  
+  // paused - shootout next round wait, break, addon break, bubble, final table prize split
+  case object Paused extends Value
+  
+}

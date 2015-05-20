ffaa79c3b71e609fb4d3e7bd54fc4d83b37e832b
Malik Baktiyarov <malik@baktiyarov.com>
2015-05-20 04:28:39 +0300

    timers

--- pokerno-engine/src/main/scala/de/pokerno/model/seat/impl/Timers.scala
+++ pokerno-engine/src/main/scala/de/pokerno/model/seat/impl/Timers.scala
@@ -7,14 +7,14 @@ import de.pokerno.model.Countdown
 trait Timers {
 
   // timer on acting (bet, discard)
-  @JsonIgnore val actingTimer       = new Countdown("acting", 30.seconds)
+  @JsonIgnore val actingTimer       = new Countdown("acting", 1.minute)
   // timer on buy in
-  @JsonIgnore val reserveTimer      = new Countdown("reserve", 30.seconds)
+  @JsonIgnore val reserveTimer      = new Countdown("reserve", 1.minute)
   // timer on reconnect
-  @JsonIgnore val reconnectTimer    = new Countdown("reconnect", 15.seconds)
+  @JsonIgnore val reconnectTimer    = new Countdown("reconnect", 30.seconds)
   // timer on disconnected
   @JsonIgnore val awayTimer         = new Countdown("away", 10.minutes)
   // timer on sit out
-  @JsonIgnore val sitOutTimer       = new Countdown("sitOut", 1.minute)
-  
+  @JsonIgnore val sitOutTimer       = new Countdown("sitOut", 30.minutes)
+
 }

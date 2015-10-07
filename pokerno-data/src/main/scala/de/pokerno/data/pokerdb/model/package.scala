298ed5b4c77d0930b56d6994cd275d4303ce98b1
Malik Baktiyarov <malik@baktiyarov.com>
2015-10-07 23:08:44 +0300

    removed pokerno-data

--- pokerno-data/src/main/scala/de/pokerno/data/pokerdb/model/package.scala
+++
@@ -1,8 +0,0 @@
-package de.pokerno.data.pokerdb
-
-package object model {
-  type UUID =  java.util.UUID
-  type Timestamp = java.sql.Timestamp
-
-  def now() = java.sql.Timestamp.from(java.time.Instant.now())
-}
